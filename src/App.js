1b90f7d19b325b79faca3473046fe0eb34f5276d
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-05-13 18:49:32 +0530

    feat : added Exercise Form , food Form and Goal form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import Exercises from './pages/Exercises';
 import Foods from './pages/Foods';
 import Goals from './pages/Goals';
+import ExerciseForm from './pages/ExerciseForm';
+import FoodForm from './pages/FoodForm';
+import GoalForm from './pages/GoalForm';
 
 function App() {
   return (
@@ -12,11 +15,17 @@ function App() {
         <Link to="/exercises">Exercises</Link>
         <Link to="/foods">Foods</Link>
         <Link to="/goals">Goals</Link>
+        <Link to="/add-exercise">Add Exercise</Link>
+        <Link to="/add-food">Add Food</Link>
+        <Link to="/add-goal">Add Goal</Link>
       </nav>
       <Routes>
         <Route path="/exercises" element={<Exercises />} />
         <Route path="/foods" element={<Foods />} />
         <Route path="/goals" element={<Goals />} />
+        <Route path='/add-exercise' element={<ExerciseForm />} />
+        <Route path='/add-food' element={<FoodForm />} />
+        <Route path='/add-goal' element={<GoalForm />} />
       </Routes>
     </div>
   );
