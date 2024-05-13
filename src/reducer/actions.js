7794ece87527d9197aff61af160d1e65161faced
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-05-13 12:03:41 +0530

    feat: added actions for add and remove

--- src/reducer/actions.js
+++ src/reducer/actions.js
@@ -5,10 +5,13 @@ const fetchExercises = () => async (dispatch) => {
   try {
     dispatch({ type: ACTIONS.FETCH_DATA_LOADING });
     const response = await fetch(HOST_URL + `/exercises`);
-    const data = await response.json();
-    dispatch({ type: ACTIONS.FETCH_EXERCISES_SUCCESS, payload: data })
+    let data = [];
+    if (response.status === 200) {
+      data = await response.json();
+    }
+    dispatch({ type: ACTIONS.FETCH_EXERCISES_SUCCESS, payload: data });
   } catch (error) {
-    console.error("Error fetching exercises data");
+    console.error('Error fetching exercises data:', error);
     dispatch({ type: ACTIONS.FETCH_EXERCISES_FAILURE });
   }
 };
@@ -17,24 +20,150 @@ const fetchFoods = () => async (dispatch) => {
   try {
     dispatch({ type: ACTIONS.FETCH_DATA_LOADING });
     const response = await fetch(HOST_URL + `/foods`);
-    const data = await response.json();
+    let data = [];
+    if (response.status === 200) {
+      data = await response.json();
+    }
     dispatch({ type: ACTIONS.FETCH_FOOD_SUCCESS, payload: data });
   } catch (error) {
-    console.error("Error fetching foods data");
+    console.error('Error fetching foods data:', error);
     dispatch({ type: ACTIONS.FETCH_FOOD_FAILURE });
   }
-}
+};
 
 const fetchGoals = () => async (dispatch) => {
   try {
     dispatch({ type: ACTIONS.FETCH_DATA_LOADING });
     const response = await fetch(HOST_URL + `/goals`);
-    const data = await response.json();
-    dispatch({ type: ACTIONS.FETCH_GOALS_SUCCESS, payload: data })
+    let data = [];
+    if (response.status === 200) {
+      data = await response.json();
+    }
+    dispatch({ type: ACTIONS.FETCH_GOALS_SUCCESS, payload: data });
   } catch (error) {
-    console.error("Error fetching Goals data");
-    dispatch({ type: ACTIONS.FETCH_GOALS_FAILURE })
+    console.error('Error fetching Goals data:', error);
+    dispatch({ type: ACTIONS.FETCH_GOALS_FAILURE });
   }
-}
+};
 
-export { fetchExercises, fetchFoods, fetchGoals };
+const removeExercise = (exerciseId) => async (dispatch) => {
+  try {
+    const response = await fetch(HOST_URL + `/exercises/${exerciseId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    // const data = response.json();
+    if (response.status === 204) {
+      console.log(response, 'remove ex');
+      dispatch({
+        type: ACTIONS.REMOVE_EXERCISE_SUCCESS,
+        payload: { exerciseId }
+      });
+    }
+  } catch (error) {
+    console.error('Error removing exercise:', error);
+    dispatch({ type: ACTIONS.REMOVE_EXERCISE_FAILURE });
+  }
+};
+
+const removeFood = (foodId) => async (dispatch) => {
+  try {
+    const response = await fetch(HOST_URL + `/foods/${foodId}`, {
+      method: 'DELETE'
+    });
+    if (response.status === 204) {
+      dispatch({ type: ACTIONS.REMOVE_FOOD_SUCCESS, payload: { foodId } });
+    }
+  } catch (error) {
+    console.error('Error removing food:', error);
+    dispatch({ type: ACTIONS.REMOVE_FOOD_FAILURE });
+  }
+};
+
+const removeGoal = (goalId) => async (dispatch) => {
+  try {
+    const response = await fetch(HOST_URL + `/goals/${goalId}`, {
+      method: 'DELETE'
+    });
+    if (response.status === 204) {
+      dispatch({ type: ACTIONS.REMOVE_GOAL_SUCCESS, payload: { goalId } });
+    }
+  } catch (error) {
+    console.error('Error removing goal:', error);
+    dispatch({ type: ACTIONS.REMOVE_GOAL_FAILURE });
+  }
+};
+
+const addExercise = (exerciseData) => async (dispatch) => {
+  try {
+    const response = await fetch(HOST_URL + `/exercises`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(exerciseData)
+    });
+    let data = {};
+    if (response.status === 201) {
+      data = await response.json();
+    }
+    dispatch({ type: ACTIONS.ADD_EXERCISE_SUCCESS, payload: data });
+  } catch (error) {
+    console.error('Error adding exercise:', error);
+    dispatch({ type: ACTIONS.ADD_EXERCISE_FAILURE });
+  }
+};
+
+const addFood = (foodData) => async (dispatch) => {
+  try {
+    const response = await fetch(HOST_URL + `/foods`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(foodData)
+    });
+    let data = {};
+    if (response.status === 201) {
+      data = await response.json();
+    }
+    dispatch({ type: ACTIONS.ADD_FOOD_SUCCESS, payload: data });
+  } catch (error) {
+    console.error('Error adding food:', error);
+    dispatch({ type: ACTIONS.ADD_FOOD_FAILURE });
+  }
+};
+
+const addGoal = (goalData) => async (dispatch) => {
+  try {
+    const response = await fetch(HOST_URL + `/goals`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(goalData)
+    });
+    let data = {};
+    if (response.status === 201) {
+      data = await response.json();
+    }
+    dispatch({ type: ACTIONS.ADD_GOAL_SUCCESS, payload: data });
+  } catch (error) {
+    console.error('Error adding goal:', error);
+    dispatch({ type: ACTIONS.ADD_GOAL_FAILURE });
+  }
+};
+
+export {
+  fetchExercises,
+  fetchFoods,
+  fetchGoals,
+  removeExercise,
+  removeFood,
+  removeGoal,
+  addExercise,
+  addFood,
+  addGoal
+};
--- src/reducer/fitnessTrackerReducer.js
+++ src/reducer/fitnessTrackerReducer.js
@@ -30,7 +30,7 @@ const fitnessTrackerReducer = (state = initialState, action) => {
     case ACTIONS.FETCH_FOOD_SUCCESS:
       return {
         ...state,
-        food: action.payload,
+        foods: action.payload,
         loading: false,
         error: null
       };
@@ -52,6 +52,78 @@ const fitnessTrackerReducer = (state = initialState, action) => {
         ...state,
         loading: false,
         error: 'Error fetching Goals data'
+      };
+    case ACTIONS.REMOVE_EXERCISE_SUCCESS:
+      console.log(action.payload.exerciseId);
+      const remainingExercises = state.exercises.filter(
+        ({ _id }) => _id !== action.payload.exerciseId
+      );
+      return {
+        ...state,
+        exercises: [...remainingExercises]
+      };
+    case ACTIONS.REMOVE_EXERCISE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: 'Error removing Exercise'
+      };
+    case ACTIONS.REMOVE_FOOD_SUCCESS:
+      const remainingFoods = state.foods.filter(
+        ({ _id }) => _id !== action.payload.foodId
+      );
+      return {
+        ...state,
+        foods: [...remainingFoods]
+      };
+    case ACTIONS.REMOVE_FOOD_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: 'Error removing Food'
+      };
+    case ACTIONS.REMOVE_GOAL_SUCCESS:
+      const remainingGoals = state.goals.filter(
+        ({ _id }) => _id !== action.payload.goalId
+      );
+      return {
+        ...state,
+        goals: [...remainingGoals]
+      };
+    case ACTIONS.REMOVE_GOAL_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: 'Error removing Goal'
+      };
+    case ACTIONS.ADD_EXERCISE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        exercises: [...state.exercises, action.payload]
+      };
+    case ACTIONS.ADD_FOOD_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        foods: [...state.foods, action.payload]
+      };
+    case ACTIONS.ADD_GOAL_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        goals: [...state.goals, action.payload]
+      };
+    case ACTIONS.ADD_EXERCISE_FAILURE:
+    case ACTIONS.ADD_FOOD_FAILURE:
+    case ACTIONS.ADD_GOAL_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: 'Error fetching or adding data'
       };
     default:
       return state;
