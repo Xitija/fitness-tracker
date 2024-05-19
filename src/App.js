7a18d66da76f6c9fe800876cfd013225932bd646
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-05-20 02:09:10 +0530

    chore : added route

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
           <Route path='/add-food' element={<FoodForm />} />
           <Route path='/add-goal' element={<GoalForm />} />
           <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/' element={<Dashboard />} />
         </Routes>
       </div>
     </div>
